e9baf4237e80d13d40a0890d5897575aecaef40e
Jhonatan Grueso <jhonper86@gmail.com>
2022-07-06 00:19:18 -0500

    Actualización del ejercicio tema 2 - java básico

--- src/main/java/org/example/Funcion.java
+++ src/main/java/org/example/Funcion.java
@@ -1,13 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Funcion {
     public static void main(String[] args) {
-        double precio = getPrecioConIVA(10000);
-        System.out.println(precio);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite el valor de un articulo: ");
+        double numero = sc.nextDouble();
+
+        double resul = getPrecioConIVA(numero);
+
+        System.out.println("El valor final con el 19% de IVA es $" + resul);
 
     }
 
     private static double getPrecioConIVA(double price) {
-        return price * 0.19;
+        double total = price + (price * 0.19);
+        return total;
     }
 }
